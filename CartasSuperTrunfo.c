5c9054e0ee97d92d91395ba7e20dfbe380b3d416
Ranisson-felix <ranissonppq@gmail.com>
2026-03-08 20:25:59 +0000

    Falta corrigir erro

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -11,24 +11,36 @@ int main() {
   int populacao,pturistico;
   float area,pib;
   char estado;
-  char cidade, carta [20];
-
-
+  char cidade[20],carta[20];
   // Área para entrada de dados
 
   printf("Escolha uma letra de 'A' a 'H', para representar o seu estado:\n ");
-  scanf("%s",&estado);
+  scanf("%c",&estado);
 
-  print("Digite o código da carta, a letra do estado seguida de um número de 01 a 04 (ex: A01, B03)\n");
+  printf("Digite o código da carta, a letra do estado seguida de um número de 01 a 04 (ex: A01, B03)\n");
   scanf("%s",&carta);
 
-  printf("Digite a quantidade de habitantes da sua cidade:\n ");
+  printf("Digite o nome da cidade:\n");
+  scanf("%s",&cidade);
+
+  printf("Digite a quantidade de habitantes da sua cidade: \n");
   scanf("%d",&populacao);
 
-  print("Digite a área em km²:\n ");
+  printf("Digite a área em km²:\n ");
   scanf("%f",&area);
+
+  printf("Digite o PIB: \n");
+  scanf("%f",&pib);
+
+  printf("Digite a quantidade de pontos turísticos; \n");
+  scanf("%d",&pturistico);
 
   // Área para exibição dos dados da cidade
 
+  printf("Carta 1");
+  printf("Estado: %s\n Código: %s\n",&estado,&carta);
+  printf("Nome da Cidade: %s\n População: %d\n",&cidade,&populacao);
+  printf("Área: %f\n PIB: %f\n",&area,&pib);
+  printf("Número de Pontos Turísticos: %d",&pturistico);
 return 0;
 } 
